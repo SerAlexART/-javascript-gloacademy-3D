fabe7ecf8e2996dc8c9aaf1e3ab603ebee6737f1
SerAlexART <wolfi-5@mail.ru>
2024-01-28 17:38:25 +0300

    Выполнение 20-ого задания

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -4,11 +4,35 @@ const modal = () => {
     const modal = document.querySelector('.popup');
     const buttons = document.querySelectorAll('.popup-btn');
     const closeBtn = modal.querySelector('.popup-close');
+    const windowWidth = document.documentElement.clientWidth;
+    console.log(windowWidth);
+    const mobileWidth = 768;
+
+    let countOpacity = 0;
+
+    // Анимация повления модального окна
+    const animationOpenModal = () => {
+        countOpacity++;
+        console.log(countOpacity);
+
+        modal.style.opacity = `${countOpacity}%`;
+
+
+        if (countOpacity < 100) {
+            setTimeout(animationOpenModal, 5);
+        }
+    };
+
 
     // Открытие модального окна при нажатии на одну из кнопок "Оставить заявку!"
     buttons.forEach((button) => {
         button.addEventListener('click', () => {
             modal.style.display = 'block';
+
+            // Запуск анимации при клике, если ширина экрана >= 768px
+            if (windowWidth >= mobileWidth) {
+                animationOpenModal();
+            }
         });
     });
 
