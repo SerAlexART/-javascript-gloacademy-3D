9900382042e3d09dff85135c622c6ab623243d05
Sergey Alekseev <s_alekseev@lesta.group>
2024-02-09 16:34:43 +0300

    Добавление конспекта 24-ого задания

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 import timer from './modules/timer';
 import menu from './modules/menu';
 import modal from './modules/modal';
+import calculator from './modules/calculator';
 // import validationAll from './modules/validationAll';
 import validationCalculator from './modules/validationCalculator';
 import validationForm from './modules/validationForm';
@@ -12,6 +13,7 @@ import slider from './modules/slider';
 timer('2 february 2024');
 menu();
 modal();
+calculator();
 // validationAll();
 validationCalculator();
 validationForm('form1');
